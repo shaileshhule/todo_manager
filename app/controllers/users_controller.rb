bc88a73fc335acebe85945a63aa729f0d23eb99e
shailesh-hule <shaileshhule@gmail.com>
2021-11-29 09:55:59 +0530

    Adde Todo and User models

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+# users_controller.rb
+class UsersController < ApplicationController
+    skip_before_action:verify_authenticity_token
+    
+    def index
+      render plain:  User.to_displayable_list
+    end 
+    
+    def show
+      id = params[:id]
+      render plain:  User.show_single(id)
+    end
+
+    def create
+      name = params[:name]
+      email = params[:email]
+      password = params[:password]
+      if User.where(email:email).present?
+        render plain: "User with #{email} is already exists."
+      else    
+        new_user = User.create!(
+          name:name,
+          email:email,
+          password:password
+        )
+        render plain:  "New user created, User id:#{new_user.id} name:#{new_user.name} email:#{new_user.email}"
+      end  
+    end
+
+    def login
+      email = params[:email]
+      password = params[:password]
+      is_exist = User.login(email:email,password:password)
+      render plain: is_exist 
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "todos", to: "todos#index"
+  post "todos", to: "todos#create"
+  get "todos/:id", to: "todos#show"
+  patch "todos/:id", to: "todos#update"
+
+  get "users", to: "users#index"
+  post "users", to: "users#create"
+  get "users/:id", to: "users#show"
+  post "users/login", to: "users#login"
+
 end
